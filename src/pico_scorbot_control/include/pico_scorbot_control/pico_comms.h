0f878415a3855a5a8e899390618514a31b8647a6
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2023-03-16 16:46:47 -0500

    Add master and slave controllers for library

--- src/pico_scorbot_control/include/pico_scorbot_control/pico_comms.h
+++ src/pico_scorbot_control/include/pico_scorbot_control/pico_comms.h
@@ -15,22 +15,25 @@ public:
     {
     }
 
-    PicoComms(const std::string &serial_device, int32_t baud_rate, int32_t timeout_ms)
-        : serial_conn_(serial_device, baud_rate, serial::Timeout::simpleTimeout(timeout_ms))
+    PicoComms(const std::string &serial_master_device, const std::string &serial_slave_device, int32_t baud_rate, int32_t timeout_ms)
+        : serial_master_conn_(serial_master_device, baud_rate, serial::Timeout::simpleTimeout(timeout_ms)), 
+        serial_slave_conn_(serial_slave_device, baud_rate, serial::Timeout::simpleTimeout(timeout_ms))
     {
     }
 
-    void setup(const std::string &serial_device, int32_t baud_rate, int32_t timeout_ms);
+    void setup(const std::string &serial_master_device, const std::string &serial_slave_device, int32_t baud_rate, int32_t timeout_ms);
     void readJointValues(std::vector<double> &pos_joints);
     void setJointValues(std::vector<double> &target_joints);
 
-    bool connected() const { return serial_conn_.isOpen(); }
+    bool connected() const { return serial_master_conn_.isOpen() && serial_slave_conn_.isOpen(); }
 
     std::string sendMsg(const std::string &msg_to_send, bool print_output = false);
-    std::vector<float> actual_pos_joints;
+    std::vector<double> actual_pos_joints;
+    std::vector<double> actual_vel_joints;
 
 private:
-    serial::Serial serial_conn_; ///< Underlying serial connection
+    serial::Serial serial_master_conn_;
+    serial::Serial serial_slave_conn_;///< Underlying serial connection
 };
 
 #endif // SCORBOT_PICO_COMMS_H
