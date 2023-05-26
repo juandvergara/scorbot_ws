ddaaac8bb66fae1ff8aeeacdd6088f0f48a43c5a
Juan D. Vergara <juan_dav.vergara@uao.edu.co>
2023-05-25 21:23:43 -0500

    Change the controller name to same name on rpi-robot

--- src/scorbot_er_v/launch/gazebo.launch.py
+++ src/scorbot_er_v/launch/gazebo.launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
-        arguments=["arm_controller"],
+        arguments=["joint_trajectory_position_controller"],
     )
 
     joint_foward_controller = Node(
